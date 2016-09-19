1e26c2e24dbe4c81343dd6146f2091db471d1b59
sidverma32 <sid.verma32@gmail.com>
2016-09-19 22:14:49 +0530

    Controller class

---
+++ AngualJSwithASP/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using AngularJSwithNET.Models;
+namespace AngularJSwithNET.Controllers
+{
+    public class ProductController : ApiController
+    {
+        // GET: api/Product
+        public IEnumerable<Product> Get()
+        {
+            ProductModel pm = new ProductModel();
+            return pm.findAll().AsEnumerable();
+        }
+
+        // GET: api/Product/5
+        public Product Get(string id)
+        {
+            ProductModel pm = new ProductModel();
+            return pm.find(id);
+        }
+    }
+}

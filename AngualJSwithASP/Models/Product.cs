9d3e020a4b588a3f17cd0e1b2e3e29cf5c49fa28
sidverma32 <sid.verma32@gmail.com>
2016-09-19 22:28:05 +0530

    Model class

---
+++ AngualJSwithASP/Models/Product.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AngularJSwithNET.Models
+{
+    public class Product
+    {
+        public string Id
+        {
+            get;
+            set;
+        }
+        public string Name
+        {
+            get;
+            set;
+        }
+        public decimal Price
+        {
+            get;
+            set;
+        }
+        public int Quantity
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ AngualJSwithASP/Models/ProductModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AngularJSwithNET.Models
+{
+    public class ProductModel
+    {
+        public List<Product>findAll()
+        {
+        List<Product> result = new List<Product>();
+        result.Add(new Product { Id = "p01", Name = "Product1", Price = 100, Quantity = 2 });
+        result.Add(new Product { Id = "p02", Name = "Product2", Price = 300, Quantity = 3 });
+        result.Add(new Product { Id = "p03", Name = "Product3", Price = 500, Quantity = 4 });
+
+
+        return result;
+        }
+        public Product find(string id)
+        {
+            return findAll().Single(p => p.Id.Equals(id));
+        }
+    }
+}
